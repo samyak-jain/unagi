a0e0c776c645ccde849af67920b10368ce6809e5
samyak-jain <samtan106@gmail.com>
2020-12-31 23:42:51 +0530

    Temporary transcoding fix

--- src/handlers/transcode.rs
+++ src/handlers/transcode.rs
@@ -15,12 +15,11 @@ pub fn start_transcoding(
     fs::create_dir_all(destination_path.clone())?;
 
     let mut destination_files = destination_path.clone();
-    destination_files.push(r#"stream_%v"#);
-    destination_files.push(r#"file%03d"#);
+    destination_files.push(r#"file%06d"#);
     destination_files.set_extension("ts");
 
     let mut destination_playlist = destination_path;
-    destination_playlist.push(r#"stream_%v"#);
+    destination_playlist.push("out");
     destination_playlist.set_extension("m3u8");
 
     let mut ffmpeg = Command::new("/usr/bin/ffmpeg");
@@ -40,7 +39,7 @@ pub fn start_transcoding(
         "h264"
     };
 
-    let filter = if should_enable_gpu {
+    let _filter = if should_enable_gpu {
         "[v:0]hwupload_cuda,split=2[vtemp001][vout002];[vtemp001]hwupload_cuda,scale_npp=w=960:h=540,hwdownload[vout001]"
     } else {
         "[v:0]split=2[vtemp001][vout002];[vtemp001]scale=w=960:h=540[vout001]"
@@ -52,12 +51,7 @@ pub fn start_transcoding(
         .args(&["-c:v", codec]) // Change video codec
         .args(&["-c:s", "webvtt"]) // Change subtitle format to webvtt
         .args(&["-crf", "20"]) // Constant rate factor. Higher is lossyer
-        .args(&["-filter_complex", filter]) // Split stream into two. Take temporary stream and downscale it
-        .args(&["-map", "[vout001]", "-b:v:0", "2000k"]) // Set bitrate for video for the 1st video stream
-        .args(&["-map", "[vout002]", "-b:v:1", "6000k"]) // Set bitrate for video for the 2nd video stream
-        .args(&["-map", "0:v", "-map", "0:a", "-map", "0:s"])
-        .args(&["-map", "0:v", "-map", "0:a", "-map", "0:s"])
-        .args(&["-var_stream_map", "v:0,a:0,s:0 v:1,a:1,s:1"])
+        .args(&["-b:v", "6000k"])
         .args(&["-force_key_frames:v", "expr:gte(t,n_forced*2.000)"])
         .args(&["-hls_time", "6"])
         .args(&["-hls_playlist_type", "event"])
@@ -65,7 +59,6 @@ pub fn start_transcoding(
             "-hls_flags",
             "delete_segments+independent_segments+discont_start+program_date_time",
         ])
-        .args(&["-master_pl_name", "master.m3u8"])
         .arg("-hls_segment_filename")
         .arg(destination_files)
         .arg(destination_playlist);
